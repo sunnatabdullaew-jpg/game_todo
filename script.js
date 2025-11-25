e4a6e4beb5c4263bb2cbff9c72a364b256319dd0
sunnatabdullaew-jpg <sunnatabdullaew@gmail.com>
2025-11-24 18:21:03 -0600

    all

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+// clearLastButton.addEventListener("click", function() {
+//     tasks.pop();
+//     FormTasks();
+// });
+
+// clearFirstButton.addEventListener("click", function() {
+//     tasks.shift();
+//     FormTasks();
+// });
+    
